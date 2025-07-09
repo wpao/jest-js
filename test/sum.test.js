e0deef4a02408af1b983fe1f5c6113cd38feae29
wpao <wpaozan@gmail.com>
2025-07-09 11:11:53 +0800

    menambahkan keterangan pengelompokan

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,12 +1,14 @@
 import { sum } from "../src/sum.js";
 
-test("test sum function 1", () => {
-  expect(sum(1, 2)).toBe(3);
-});
-test("test sum function 2", () => {
-  expect(sum(1, 2)).toBe(3);
-});
-// jangan ada oprator
-test("test sum function 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("matching cities to foods", () => {
+  test("test sum function 1", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  test("test sum function 2", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  // jangan ada oprator
+  test("test sum function 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
